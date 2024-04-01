fcbaa80a168188c3fb7cd65f60b10c22ffd5beb5
tosdevil <41502136+tosdevil@users.noreply.github.com>
2024-04-01 06:23:15 +0300

    приложение вырезает фон у фотографий, загруженных в бд, фронта ещё нет

--- cutdabackground/cutdabackground/settings.py
+++ cutdabackground/cutdabackground/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+		'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ cutdabackground/main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from rembg import remove
+from PIL import Image
+import os
+from django.core.files.base import ContentFile
+
+# Create your models here.
+
+class ImageCut(models.Model):
+	img = models.ImageField(upload_to="images")
+	out_img = models.ImageField(blank=True, upload_to="images")
+
+	def __str__(self):
+		return str(self.id)
+	
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
+
+		if self.img and not self.out_img:
+			input_image_path = self.img.path
+			output_image_path = f"{self.img.name.split('/')[-1].split('.')[0]}_out.png"  # Имя файла для выходного изображения
+
+			with open(input_image_path, "rb") as f:
+					image_data = f.read()
+
+			processed_image_data = remove(image_data)
+
+			# Сохранение обработанного изображения
+			with open(output_image_path, "wb") as f:
+					f.write(processed_image_data)
+
+			self.out_img.save(f"{output_image_path}", ContentFile(processed_image_data), save=False)
+			super().save(*args, **kwargs)
